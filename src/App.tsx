a709e6f112d62b6e857a2da5d67011f0f6f874e8
yualta <altamirano@cicese.edu.mx>
2024-01-31 16:00:22 +0100

    modified:   src/App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,114 +1,137 @@
 import React, { ChangeEvent, useEffect, useState } from 'react';
 import './App.css';
 import { Canvas, useFrame, useGraph } from '@react-three/fiber';
-import { Color, Euler, Matrix4, SkinnedMesh } from 'three';
-import { useGLTF } from '@react-three/drei';
-import { Category, FaceLandmarker, FilesetResolver } from '@mediapipe/tasks-vision';
+import { Color, Euler, Matrix4, Quaternion, SkinnedMesh } from 'three';
+import { PositionPoint, useGLTF } from '@react-three/drei';
+import { Category, FaceLandmarker,FaceLandmarkerOptions, FilesetResolver } from '@mediapipe/tasks-vision';
 import { useDropzone } from 'react-dropzone';
+import { Console } from 'console';
+
 
 let video: HTMLVideoElement;
 let faceLandmarker: FaceLandmarker;
-let lastVideoTime= -1;
-let headMesh: SkinnedMesh;
+let lastVideoTime = -1;
+let blendshapes: any[] = [];
 let rotation: Euler;
-let blendshapes: Category[] = []
+let headMesh: any[] = [];
+let position: number[]= [];
+
+const options: FaceLandmarkerOptions = {
+  baseOptions: {
+    modelAssetPath: `https://storage.googleapis.com/mediapipe-models/face_landmarker/face_landmarker/float16/1/face_landmarker.task`,
+    delegate: "GPU"
+  },
+  numFaces: 1,
+  runningMode: "VIDEO",
+  outputFaceBlendshapes: true,
+  outputFacialTransformationMatrixes: true,
+};
+
+function Avatar({ url }: { url: string }) {
+  const { scene } = useGLTF(url);
+  const { nodes } = useGraph(scene);
+  //console.log(nodes);
+
+  useEffect(() => {
+    if (nodes.Wolf3D_Head) headMesh.push(nodes.Wolf3D_Head);
+    if (nodes.Wolf3D_Teeth) headMesh.push(nodes.Wolf3D_Teeth);
+    if (nodes.Wolf3D_Beard) headMesh.push(nodes.Wolf3D_Beard);
+    if (nodes.Wolf3D_Avatar) headMesh.push(nodes.Wolf3D_Avatar);
+    if (nodes.Wolf3D_Head_Custom) headMesh.push(nodes.Wolf3D_Head_Custom);
+    if (nodes.Wolf3D_LeftShoulder) headMesh.push(nodes.Wolf3D_LeftShoulder);
+  }, [nodes, url]);
+
+  useFrame(() => {
+    if (blendshapes.length > 0) {
+      blendshapes.forEach(element => {
+        headMesh.forEach(mesh => {
+          let index = mesh.morphTargetDictionary[element.categoryName];
+          if (index >= 0) {
+            mesh.morphTargetInfluences[index] = element.score;
+          }
+        });
+      });
+
+      nodes.Head.rotation.set(rotation.x, rotation.y, rotation.z);
+      nodes.Neck.rotation.set(rotation.x / 5 + 0.3, rotation.y / 5, rotation.z / 5);
+      nodes.Spine1.rotation.set(rotation.x / 10, rotation.y / 10, rotation.z / 10);
+      //x: 0.06222337856888771, y: 0.10491103678941727, z: -0.012486680410802364
+      //nodes.LeftShoulder.position.set(position[0],position[1],position[2]);
+      //nodes.LeftShoulder.rotation.set(rotation.x -rotation.x,rotation.y,rotation.z);
+      //console.log("HEAD:", nodes.Head.rotation.set(rotation.x, rotation.y, rotation.z));
+
+    }
+  });
+
+  return <primitive object={scene} position={[0, -1.75, 3]} />
+}
 
 function App() {
-  const [url,setUrl] = useState<string>("https://models.readyplayer.me/65b7bfcb9cc25bfa650d7331.glb")
-  const handleOnChange = (event: any) => {
-    
-    setUrl(event.target.value)
-  }
+  const [url, setUrl] = useState<string>("https://models.readyplayer.me/65b7bfcb9cc25bfa650d7331.glb?morphTargets=ARKit&textureAtlas=1024");
+  const { getRootProps } = useDropzone({
+    onDrop: files => {
+      const file = files[0];
+      const reader = new FileReader();
+      reader.onload = () => {
+        setUrl(reader.result as string);
+      }
+      reader.readAsDataURL(file);
+    }
+  });
 
   const setup = async () => {
-    const vision = await FilesetResolver.forVisionTasks("https://cdn.jsdelivr.net/npm/@mediapipe/tasks-vision@latest/wasm");
+    const filesetResolver = await FilesetResolver.forVisionTasks("https://cdn.jsdelivr.net/npm/@mediapipe/tasks-vision@0.10.0/wasm");
+    faceLandmarker = await FaceLandmarker.createFromOptions(filesetResolver, options);
 
-    faceLandmarker = await FaceLandmarker.createFromOptions(
-        vision,
-        {
-          baseOptions: {
-            modelAssetPath: `https://storage.googleapis.com/mediapipe-models/face_landmarker/face_landmarker/float16/1/face_landmarker.task`,
-            delegate: "GPU"
-          },
-          outputFaceBlendshapes: true,
-          outputFacialTransformationMatrixes:true,
-          runningMode: "VIDEO"
-        });
-
-
-    video= document.getElementById("video") as HTMLVideoElement;
+    video = document.getElementById("video") as HTMLVideoElement;
     navigator.mediaDevices.getUserMedia({
-      video: { width: 1280, height: 720},
-      audio:false
-    }).then((stream) => {
+      video: { width: 1280, height: 720 },
+      audio: false,
+    }).then(function (stream) {
       video.srcObject = stream;
       video.addEventListener("loadeddata", predict);
-
-    })
+    });
   }
 
-  const predict = () => {
-    const nowIsMs = Date.now();
+  const predict = async () => {
+    let nowInMs = Date.now();
     if (lastVideoTime !== video.currentTime) {
-        lastVideoTime = video.currentTime;
-        const result = faceLandmarker.detectForVideo(video,nowIsMs)
-        
-        if(result.facialTransformationMatrixes && result.facialTransformationMatrixes.length > 0  && result.faceBlendshapes &&  result.faceBlendshapes.length > 0){
-          const matrix = new Matrix4().fromArray(result.facialTransformationMatrixes[0].data);
-          rotation = new Euler().setFromRotationMatrix(matrix);
+      lastVideoTime = video.currentTime;
+      const faceLandmarkerResult = faceLandmarker.detectForVideo(video, nowInMs);
 
-          blendshapes = result.faceBlendshapes[0].categories;
-        }   
+      if (faceLandmarkerResult.faceBlendshapes && faceLandmarkerResult.faceBlendshapes.length > 0 && faceLandmarkerResult.faceBlendshapes[0].categories) {
+        blendshapes = faceLandmarkerResult.faceBlendshapes[0].categories;
+
+        const matrix = new Matrix4().fromArray(faceLandmarkerResult.facialTransformationMatrixes![0].data);
+        rotation = new Euler().setFromRotationMatrix(matrix);
+
+      }
     }
-    
-    requestAnimationFrame(predict);
+
+    window.requestAnimationFrame(predict);
   }
 
-  useEffect(() =>{
+  const handleOnChange = (event: any) => {
+    setUrl(`${event.target.value}?morphTargets=ARKit&textureAtlas=1024`);
+  }
+
+  useEffect(() => {
     setup();
-  },[])
+  }, []);
 
   return (
     <div className="App">
-      <input type='text' placeholder='Ingresa el rpm de tu avatar' onChange={handleOnChange}/>
-      <video autoPlay id='video'/>
-      <Canvas style={{backgroundColor:'1a0321',
-        height:400,
-        }}
-          camera={{
-            fov:25
-        }}>
-        <ambientLight intensity={0.5}/>
-        <pointLight position={[1,1,1]} color={new Color(1,0,0)} intensity={0.5}/>
-        <pointLight position={[-1,0,1]} color={new Color(0,1,0)} intensity={0.5}/>
-        <Avatar url={url}/>
+      <input className='url' type="text" placeholder="Paste RPM avatar URL" onChange={handleOnChange} />
+      <p></p>
+      <video className='camera-feed' id="video" autoPlay></video>
+      <Canvas style={{ height: 600 }} camera={{ fov: 25 }} shadows>
+        <ambientLight intensity={0.5} />
+        <pointLight position={[10, 10, 10]} color={new Color(1, 1, 0)} intensity={0.5} castShadow />
+        <pointLight position={[-10, 0, 10]} color={new Color(1, 0, 0)} intensity={0.5} castShadow />
+        <pointLight position={[0, 0, 10]} intensity={0.5} castShadow />
+        <Avatar url={url} />
       </Canvas>
     </div>
   );
-}
-
-function Avatar({url}:{url: string}){
-  const avatar = useGLTF(`${url}?morphTargets=ARKit&textureAtlas=1024`)
-  const {nodes} = useGraph(avatar.scene)
-  
-  useEffect ( () =>{
-    headMesh = nodes.Wolf3D_Avatar as SkinnedMesh
-  },[nodes])
-
-  useFrame((_,delta) => {
-    if(headMesh !== null){
-      blendshapes.forEach((blendshape) => {
-        let index = headMesh.morphTargetDictionary![blendshape.categoryName];       
-        if(index >= 0){
-          headMesh.morphTargetInfluences![index] = blendshape.score;
-        }
-      })
-    }
-    //nodes.Head.rotation.set(rotation.x/3, rotation.y/3, rotation.z/3);
-    //nodes.Neck.rotation.set(rotation.x/3, rotation.y/3, rotation.z/3);
-    //nodes.Spine1.rotation.set(rotation.x/3 ,rotation.y/3 , rotation.z/3);
-  })
-
-  return <primitive object={avatar.scene} position={[0,-1.60,4]}/>
 }
 export default App;
